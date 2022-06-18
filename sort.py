d73cf58cc94b4a6ab21ce85b99a2c4620fe11d7a
Gustavo Barros <gbarrospe@gmail.com>
2022-06-18 20:06:22 -0300

    add inserction sort and rename file

--- sort.py
+++ sort.py
@@ -7,6 +7,15 @@ def selection_sort(arr):
                 min_idx = i
         (arr[step],arr[min_idx])=(arr[min_idx],arr[step])
 
+def insertion_sort(arr):
+    n = len(arr)
+    for step in range(1,n):
+        stepValue = arr[step]
+        j = step - 1
+        while j>=0 and arr[j] > stepValue:
+            arr[j+1]=arr[j]
+            j = j -1
+        arr[j+1]=stepValue
 
 
 def print_list(arr):
@@ -16,5 +25,6 @@ arr = [3,8,4,7,1,5]
 #arr=[5]
 #arr=[]
 print_list(arr)
-selection_sort(arr)
+#selection_sort(arr)
+insertion_sort(arr)
 print_list(arr)
