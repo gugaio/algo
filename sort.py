db5183d77d848f478bf7d6ffed4ac82b126b085d
Gustavo Barros <gbarrospe@gmail.com>
2022-06-18 23:45:56 -0300

    add bubble sorting

--- sort.py
+++ sort.py
@@ -7,6 +7,7 @@ def selection_sort(arr):
                 min_idx = i
         (arr[step],arr[min_idx])=(arr[min_idx],arr[step])
 
+
 def insertion_sort(arr):
     n = len(arr)
     for step in range(1,n):
@@ -18,13 +19,26 @@ def insertion_sort(arr):
         arr[j+1]=stepValue
 
 
+def bubble_sort(arr):
+    n = len(arr)
+    for step in range(n-1):
+        for j in range(n-1-step):
+            if arr[j]>arr[j+1]:
+                (arr[j],arr[j+1])=(arr[j+1],arr[j])
+                swapped=True
+        if not swapped:
+            return
+
+
 def print_list(arr):
     print(arr)
+
 
 arr = [3,8,4,7,1,5]
 #arr=[5]
 #arr=[]
 print_list(arr)
 #selection_sort(arr)
-insertion_sort(arr)
+#insertion_sort(arr)
+bubble_sort(arr)
 print_list(arr)
