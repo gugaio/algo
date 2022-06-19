2490a2662b7603aa0c86059b82d5094f3e9fa109
gugaio <gbarrospe@gmail.com>
2022-06-19 14:10:25 -0300

    add merge sorting

--- sort.py
+++ sort.py
@@ -1,3 +1,36 @@
+def merge_sort(arr):
+    if len(arr)<=1:
+        return
+    
+    mid = len(arr)//2
+    
+    left = arr[mid:]
+    right = arr[:mid]
+    
+    merge_sort(left)
+    merge_sort(right)
+    
+    i=j=k=0
+    while i < len(left) and j < len(right):
+        if left[i]<right[j]:
+            arr[k]=left[i]
+            i += 1
+        else:
+            arr[k]=right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k]=left[i]
+        k += 1
+        i += 1
+
+    while j < len(right):
+        arr[k]=right[j]
+        k += 1
+        j += 1
+
+
 def selection_sort(arr):
     n = len(arr)
     for step in range(n):
@@ -40,5 +73,6 @@ arr = [3,8,4,7,1,5]
 print_list(arr)
 #selection_sort(arr)
 #insertion_sort(arr)
-bubble_sort(arr)
+#bubble_sort(arr)
+merge_sort(arr)
 print_list(arr)
